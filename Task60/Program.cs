c513e47ff5608f1d3df7f3e9799be2119034931c
Irina <avis.tenera@gmail.com>
2022-09-04 16:13:18 +0300

    Добавили задачу 60

---
+++ Task60/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] FillMatrix(int xCount, int yCount, int zCount)
+{
+    int temp = 10;
+    int[,,] matrix = new int[xCount, yCount, zCount];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = temp;
+                temp++;
+            }
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+int [,,] matrix = FillMatrix(2,2,2);
+PrintMatrix(matrix);
