db8f8465b9d76bd817b78111341e58b9d52a5f3e
Irina <avis.tenera@gmail.com>
2022-09-04 16:11:35 +0300

    Добавили задачу 54

---
+++ Task54/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой 
+// строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int[,] FillMatrix(int rowsCount, int columnsCount)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void RowDescending(int[,] matrix)
+{
+    int min = matrix[0, 0];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                int temp = 0;
+
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                     matrix[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+Console.Write("Введите m и n через Enter ");
+int m = Convert.ToInt32(Console.ReadLine());//число строк
+int n = Convert.ToInt32(Console.ReadLine());//число столбцов
+
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+
+RowDescending(matrix);
+Console.WriteLine();
+
+PrintMatrix(matrix);
